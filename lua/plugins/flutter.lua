aa346a9546cf9c5afc97a7aed8d120330fcf9913
blackh-t <nache7383@uit.no>
2025-04-12 04:16:33 +0200

    + auto formatter Flutter

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -4,8 +4,17 @@ return {
     lazy = false,
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'stevearc/dressing.nvim', -- optional for vim.ui.select
     },
-    config = true,
+    config = function()
+      require('flutter-tools').setup {}
+
+      -- Autoformat ved lagring
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*.dart',
+        callback = function()
+          vim.lsp.buf.format { async = false }
+        end,
+      })
+    end,
   },
 }
